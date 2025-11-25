2fa9ae9fa74e904aad8117df6cf5e46e58773ec4
Thomas <thomas.biot123@gmail.com>
2025-11-25 11:04:01 +0100

    ajustement de l'inclusion du script dans le html et ajout des fonctionnalités JS (bouton fonctionnel qui appelle la fonction qui ajoute les éléments dans le tableau et qui appelle la fonction qui affiche le contenu du tableau)

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const taskInput = document.getElementById("taskInput");
-const addTaskBtn = document.getElementById("taskBtn");
+const taskBtn = document.getElementById("taskBtn");
 const taskList = document.getElementById("taskList");
 
 let tasks = [];
+
+function renderTasks() {
+    taskList.innerHTML = ""; 
+
+    for (let i = 0; i < tasks.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = tasks[i];
+        taskList.appendChild(li);
+    }
+}
+
+function addTask() {
+    const newTask = taskInput.value;
+    tasks.push(newTask);
+    taskInput.value = "";
+    renderTasks()
+}
+
+taskBtn.addEventListener("click", addTask)
