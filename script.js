e816cb3b9453d081e9690c77a30ce4cbeab02188
Thomas <thomas.biot123@gmail.com>
2025-11-25 15:12:45 +0100

    ajout du bouton delete

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ function renderTasks() {
     for (let i = 0; i < tasks.length; i++) {
         let li = document.createElement("li");
 
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.className = "delete-btn";
+
+        deleteBtn.addEventListener("click", function () {
+            deleteTask(i);
+        });
+
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox"
         checkbox.checked = tasks[i].done;
@@ -21,6 +29,7 @@ function renderTasks() {
 
         li.textContent = tasks[i].text;
         li.appendChild(checkbox);
+        li.appendChild(deleteBtn)
         taskList.appendChild(li);
     }
 }
@@ -35,6 +44,12 @@ function addTask() {
         taskInput.value = "";
         renderTasks()
     }
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+
+    renderTasks();
 }
 
 taskBtn.addEventListener("click", addTask)
