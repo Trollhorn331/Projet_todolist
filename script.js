3dc00a9357cc632d17335e4f9f29811fdd3e6c24
Thomas <thomas.biot123@gmail.com>
2025-11-25 10:21:38 +0100

    début du script + mise à jour du html pour exécuter le js

---
+++ script.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("taskBtn");
+const taskList = document.getElementById("taskList");
+
+let tasks = [];
